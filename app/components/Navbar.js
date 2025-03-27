123695a583584623d91ff37b7fabfe2906983ece
UMANG-2003 <umangkumar20003@gmail.com>
2025-03-27 18:05:35 +0530

    #

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,22 +1,23 @@
 import Link from "next/link";
+import Image from "next/image";
 import React from "react";
 
-function Navbar({navRef}) {
-
+function Navbar({ navRef }) {
   const menu = [
     { name: "HOME", link: "/" },
     { name: "ABOUT", link: "/about" },
     { name: "SERVICES", link: "/services" },
     { name: "CONTACT", link: "/contact" },
   ];
+
   return (
     <div
       ref={navRef}
       className="nav bg-black text-white p-4 flex justify-between items-center fixed top-0 w-full z-50 border-b-2 border-gray-800"
     >
       <div className="logo flex items-center gap-2">
-        <img src="/logo.png" className="w-10" alt="" />
-        <p className=" text-lg boldonse-regular">UMANG</p>
+        <Image src="/logo.png" width={40} height={40} alt="UMANG logo" />
+        <p className="text-lg boldonse-regular">UMANG</p>
       </div>
 
       <div className="menu max-md:hidden">
@@ -29,13 +30,14 @@ function Navbar({navRef}) {
           </Link>
         ))}
       </div>
+
       <a href="https://github.com/UMANG-2003" className="max-md:hidden">
         <button
           type="button"
           className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 cursor-pointer"
         >
           <span className="flex items-center gap-2">
-            <img src="/github.png" className="w-7" alt="" />
+            <Image src="/github.png" width={28} height={28} alt="GitHub logo" />
             <p>GitHub</p>
           </span>
         </button>
